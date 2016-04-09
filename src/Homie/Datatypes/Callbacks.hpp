7e4a19bd71aa38cb6b2e6be544638dbacc29985f
Ronald Schaten <ronald@schatenseite.de>
2016-04-10 00:34:59 +0200

    replaced function pointer with function object

--- src/Homie/Datatypes/Callbacks.hpp
+++ src/Homie/Datatypes/Callbacks.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "../../HomieEvent.h"
+#include <functional>
 
 namespace HomieInternals {
   typedef void (*OperationFunction)();
 
   typedef bool (*GlobalInputHandler)(String nodeId, String property, String value);
   typedef bool (*NodeInputHandler)(String property, String value);
-  typedef bool (*PropertyInputHandler)(String value);
+  typedef std::function<bool(String)> PropertyInputHandler;
 
   typedef void (*EventHandler)(HomieEvent event);
 
