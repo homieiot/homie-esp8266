e871a2d9c4bad82f88b4826e7cfa010f8dafe4be
Marvin Roger <bonjourmarvin@marvinroger.fr>
2016-04-14 10:17:04 +0200

    Make some private HomieNode functions actually private with friendship

--- src/HomieNode.h
+++ src/HomieNode.h
@@ -6,19 +6,28 @@
 #include "Homie/Datatypes/Callbacks.hpp"
 #include "Homie/Limits.hpp"
 
+namespace HomieInternals {
+  class HomieClass;
+  class BootNormal;
+  class BootConfig;
+}
+
 class HomieNode {
+  friend HomieInternals::HomieClass;
+  friend HomieInternals::BootNormal;
+  friend HomieInternals::BootConfig;
   public:
     HomieNode(const char* id, const char* type, HomieInternals::NodeInputHandler nodeInputHandler = [](String property, String value) { return false; });
 
     void subscribe(const char* property, HomieInternals::PropertyInputHandler inputHandler = [](String value) { return false; });
 
+  private:
     const char* getId();
     const char* getType();
     HomieInternals::Subscription* getSubscriptions();
     unsigned char getSubscriptionsCount();
     HomieInternals::NodeInputHandler getInputHandler();
 
-  private:
     const char* _id;
     const char* _type;
     HomieInternals::Subscription _subscriptions[HomieInternals::MAX_SUBSCRIPTIONS_COUNT_PER_NODE];
