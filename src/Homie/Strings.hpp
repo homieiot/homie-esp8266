e32a47b8d319086c8a0a773f8281dff17255b123
Marvin Roger <marvinroger@users.noreply.github.com>
2016-09-03 19:48:29 +0200

    Attempt to fix CORS issue #48

--- src/Homie/Strings.hpp
+++ src/Homie/Strings.hpp
@@ -3,7 +3,7 @@
 namespace HomieInternals {
   // config mode
 
-  const char PROGMEM_CONFIG_CORS[] PROGMEM = "HTTP/1.1 204 No Content\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: PUT\r\nAccess-Control-Allow-Headers: Content-Type\r\n\r\n";
+  const char PROGMEM_CONFIG_CORS[] PROGMEM = "HTTP/1.1 204 No Content\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: PUT,POST\r\nAccess-Control-Allow-Headers: Content-Type,Accept,Origin\r\n\r\n";
   const char PROGMEM_CONFIG_APPLICATION_JSON[] PROGMEM = "application/json";
   const char PROGMEM_CONFIG_JSON_FAILURE_BEGINNING[] PROGMEM = "{\"success\":false,\"error\":\"";
   const char PROGMEM_CONFIG_NETWORKS_FAILURE[] PROGMEM = "{\"error\": \"Initial Wi-Fi scan not finished yet\"}";
