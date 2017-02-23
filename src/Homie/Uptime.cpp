8704d04826cfdf93d14205d0004b0ebfb45ccc09
FuryFire <furyfire@users.noreply.github.com>
2017-02-23 15:45:18 +0100

    Fix for overflow on millis() counter, #299

--- src/Homie/Uptime.cpp
+++ src/Homie/Uptime.cpp
@@ -4,15 +4,20 @@ using namespace HomieInternals;
 
 Uptime::Uptime()
 : _seconds(0)
-, _lastTick(0) {
+, _lastTick(0)
+, _rolloverCount(0)   {
 }
 
 void Uptime::update() {
-  uint64_t now = millis();
-  _seconds += (now - _lastTick) / 1000UL;
+  uint32_t now = millis();
+  
+  if (now < _lastTick) {
+    _rolloverCount++;
+  }
   _lastTick = now;
+  _seconds =  (0xFFFFFFFF / 1000 ) * _rolloverCount + (_lastTick / 1000);
 }
 
-uint64_t Uptime::getSeconds() const {
+uint32_t Uptime::getSeconds() const {
   return _seconds;
 }
