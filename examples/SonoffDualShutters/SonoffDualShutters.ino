24a2f35dde59db6e49ebb6fecafce1d0ca0ea042
Lübbe <luebbe.opensource@gmail.com>
2019-08-27 10:44:43 +0200

    Fix SonoffDualShutters example

--- examples/SonoffDualShutters/SonoffDualShutters.ino
+++ examples/SonoffDualShutters/SonoffDualShutters.ino
@@ -21,7 +21,8 @@ if level != 0 or open shutters if level == 0
 #include <SonoffDual.h>
 #include <Shutters.h>
 
-const unsigned long COURSE_TIME = 30 * 1000;
+const unsigned long UPCOURSE_TIME = 30 * 1000;
+const unsigned long DOWNCOURSE_TIME = 30 * 1000;
 const float CALIBRATION_RATIO = 0.1;
 
 const bool RELAY1_MOVE = true;
@@ -36,55 +37,81 @@ HomieNode shuttersNode("shutters", "shutters");
 
 // Shutters
 
-void shuttersUp() {
-  SonoffDual.setRelays(RELAY1_MOVE, RELAY2_UP);
+void shuttersOperationHandler(Shutters *shutters, ShuttersOperation operation)
+{
+  switch (operation)
+  {
+  case ShuttersOperation::UP:
+    SonoffDual.setRelays(RELAY1_MOVE, RELAY2_UP);
+    break;
+  case ShuttersOperation::DOWN:
+    SonoffDual.setRelays(RELAY1_MOVE, RELAY2_DOWN);
+    break;
+  case ShuttersOperation::HALT:
+    SonoffDual.setRelays(RELAY1_STOP, false);
+    break;
+  }
 }
 
-void shuttersDown() {
-  SonoffDual.setRelays(RELAY1_MOVE, RELAY2_DOWN);
+void shuttersWriteStateHandler(Shutters *shutters, const char *state, byte length)
+{
+  for (byte i = 0; i < length; i++)
+  {
+    EEPROM.write(SHUTTERS_EEPROM_POSITION + i, state[i]);
+#ifdef ESP8266
+    EEPROM.commit();
+#endif
+  }
 }
 
-void shuttersHalt() {
-  SonoffDual.setRelays(RELAY1_STOP, false);
+void readFromEeprom(char *dest, byte length)
+{
+  for (byte i = 0; i < length; i++)
+  {
+    dest[i] = EEPROM.read(SHUTTERS_EEPROM_POSITION + i);
+  }
 }
 
-uint8_t shuttersGetState() {
-  return EEPROM.read(SHUTTERS_EEPROM_POSITION);
+void onShuttersLevelReached(Shutters *shutters, uint8_t level)
+{
+  if (shutters->isIdle())
+    Homie.setIdle(true); // if idle, we've reached our target
+  if (Homie.isConnected())
+    shuttersNode.setProperty("level").send(String(level));
 }
 
-void shuttersSetState(uint8_t state) {
-  EEPROM.write(SHUTTERS_EEPROM_POSITION, state);
-  EEPROM.commit();
-}
-
-Shutters shutters(COURSE_TIME, shuttersUp, shuttersDown, shuttersHalt, shuttersGetState, shuttersSetState, CALIBRATION_RATIO, onShuttersLevelReached);
-
-void onShuttersLevelReached(uint8_t level) {
-  if (shutters.isIdle()) Homie.setIdle(true); // if idle, we've reached our target
-  if (Homie.isConnected()) shuttersNode.setProperty("level").send(String(level));
-}
+Shutters shutters;
 
 // Homie
 
-void onHomieEvent(const HomieEvent& event) {
-  switch (event.type) {
-    case HomieEventType::ABOUT_TO_RESET:
-      shutters.reset();
-      break;
+void onHomieEvent(const HomieEvent &event)
+{
+  switch (event.type)
+  {
+  case HomieEventType::ABOUT_TO_RESET:
+    shutters.reset();
+    break;
+  default:
+    break;
   }
 }
 
-bool shuttersLevelHandler(const HomieRange& range, const String& value) {
-  for (byte i = 0; i < value.length(); i++) {
-    if (isDigit(value.charAt(i)) == false) return false;
+bool shuttersLevelHandler(const HomieRange &range, const String &value)
+{
+  for (byte i = 0; i < value.length(); i++)
+  {
+    if (isDigit(value.charAt(i)) == false)
+      return false;
   }
 
   const unsigned long numericValue = value.toInt();
-  if (numericValue > 100) return false;
+  if (numericValue > 100)
+    return false;
 
   // wanted value is valid
 
-  if (shutters.isIdle() && numericValue == shutters.getCurrentLevel()) return true; // nothing to do
+  if (shutters.isIdle() && numericValue == shutters.getCurrentLevel())
+    return true; // nothing to do
 
   Homie.setIdle(false);
   shutters.setLevel(numericValue);
@@ -94,7 +121,8 @@ bool shuttersLevelHandler(const HomieRange& range, const String& value) {
 
 // Logic
 
-void setup() {
+void setup()
+{
   SonoffDual.setup();
   EEPROM.begin(4);
   shutters.begin();
@@ -105,23 +133,42 @@ void setup() {
   Homie.setLedPin(SonoffDual.LED_PIN, SonoffDual.LED_ON);
   Homie.onEvent(onHomieEvent);
 
+  char storedShuttersState[shutters.getStateLength()];
+  readFromEeprom(storedShuttersState, shutters.getStateLength());
+
+  shutters
+      .setCourseTime(UPCOURSE_TIME, DOWNCOURSE_TIME)
+      .setCalibrationRatio(CALIBRATION_RATIO)
+      .setOperationHandler(shuttersOperationHandler)
+      .setWriteStateHandler(shuttersWriteStateHandler)
+      .onLevelReached(onShuttersLevelReached)
+      .restoreState(storedShuttersState)
+      .begin();
+
   shuttersNode.advertise("level").settable(shuttersLevelHandler);
 
   Homie.setup();
 }
 
-void loop() {
+void loop()
+{
   shutters.loop();
   Homie.loop();
   SonoffDualButton buttonState = SonoffDual.handleButton();
-  if (buttonState == SonoffDualButton::LONG) {
+  if (buttonState == SonoffDualButton::LONG)
+  {
     Homie.reset();
-  } else if (buttonState == SonoffDualButton::SHORT && shutters.isIdle()) {
+  }
+  else if (buttonState == SonoffDualButton::SHORT && shutters.isIdle())
+  {
     Homie.setIdle(false);
 
-    if (shutters.getCurrentLevel() == 100) {
+    if (shutters.getCurrentLevel() == 100)
+    {
       shutters.setLevel(0);
-    } else {
+    }
+    else
+    {
       shutters.setLevel(100);
     }
   }
