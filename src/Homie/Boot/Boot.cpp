a02434b56c0fc144ca28e85b7fbf74423d7714e6
Markus Frey <mfrey@mailbox.org>
2020-01-02 19:51:35 +0100

    Add workaround for AP crash issues (#619)

--- src/Homie/Boot/Boot.cpp
+++ src/Homie/Boot/Boot.cpp
@@ -13,7 +13,11 @@ void Boot::setup() {
     digitalWrite(Interface::get().led.pin, !Interface::get().led.on);
   }
 
+  #ifdef ESP32
+  WiFi.persistent(false); // Workaround for ESP32: Disable Wi-Fi persistence to prevent crashes on AP connect
+  #else
   WiFi.persistent(true);  // Persist data on SDK as it seems Wi-Fi connection is faster
+  #endif //ESP32
 
   Interface::get().getLogger() << F("💡 Firmware ") << Interface::get().firmware.name << F(" (") << Interface::get().firmware.version << F(")") << endl;
   Interface::get().getLogger() << F("🔌 Booting into ") << _name << F(" mode 🔌") << endl;
